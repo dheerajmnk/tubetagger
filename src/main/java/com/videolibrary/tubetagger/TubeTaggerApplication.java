6577285348e5e073a14551b491656db0dfafb597
dheerajmnk <dheerajmenonk@gmail.com>
2025-11-07 22:52:53 +0530

    Project setup and homepage routing

---
+++ src/main/java/com/videolibrary/tubetagger/TubeTaggerApplication.java
@@ -0,0 +1,13 @@
+package com.videolibrary.tubetagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubeTaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubeTaggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/videolibrary/tubetagger/TubeTaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videolibrary.tubetagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubetaggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
