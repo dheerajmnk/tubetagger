ec588b8a50a801a403803d1217aee8afe0e8d2c1
dheerajmnk <dheerajmenonk@gmail.com>
2025-11-23 16:06:03 +0530

    DDL scripts updated

--- src/main/resources/tubetagger_schema.sql
+++ src/main/resources/tubetagger_schema.sql
@@ -1,25 +1,25 @@
-CREATE TABLE if NOT EXISTS video (
-	video_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	url VARCHAR(100) NOT NULL,
-	title VARCHAR(200) NOT NULL
-);
-
-ALTER TABLE video ADD CONSTRAINT unique_youtube_url UNIQUE (url);
-
-ALTER TABLE video ADD COLUMN thumbnail VARCHAR(100) NOT NULL;
-
 CREATE TABLE if NOT EXISTS channel (
   channel_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );
 
-ALTER TABLE video ADD COLUMN channel_id INT;
-
-ALTER TABLE video ADD CONSTRAINT fk_video_channel FOREIGN KEY (channel_id) REFERENCES channel (channel_id) ON DELETE SET NULL;
-
 CREATE TABLE if NOT EXISTS category (
   category_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS video (
+    video_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    url VARCHAR(100) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    thumbnail VARCHAR(100) NOT NULL,
+    channel_id INT,
+
+    CONSTRAINT unique_youtube_url UNIQUE (url),
+    CONSTRAINT fk_video_channel
+        FOREIGN KEY (channel_id)
+        REFERENCES channel (channel_id)
+        ON DELETE SET NULL
 );
 
 CREATE TABLE video_category (
